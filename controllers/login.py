3cbaf86504be3db64de5e3c33c155eb1ed1af263
the-alex <thealex@umich.edu>
2015-01-17 07:45:19 -0500

    DON'T BREAK

--- controllers/login.py
+++ controllers/login.py
@@ -3,7 +3,11 @@ import os
 
 login = Blueprint('login', __name__, template_folder='templates')
 
+def add_user():
+    pass
+
 @login.route('/login', methods=['GET', 'POST'])
 def login_route():
-    return render_template("login.html")
-
+    test = 5
+    options = {'display': test}
+    return render_template("login.html", **options)
--- parse_tests/ptest.py
+++ parse_tests/ptest.py
@@ -1,8 +1,15 @@
-import keys
-import parse_rest
 from parse_rest.connection import register
+from parse_rest.user import User as ParseUser
 from parse_rest.datatypes import Object as ParseObject
 
-register(keys.APPLICATION_ID, keys.REST_API_KEY, master_key=None)
+APPLICATION_ID = "HC69OFTmEYGO6uXqiNzL8SwDX4vzg8fikXul8F39"
+REST_API_KEY = "HYOrBxaYg23WNM1p7c9iLjMedFjWpMud6pNQrdUE"
+MASTER_KEY = "nneshWYCXhVtaJWlRjzXxUkl5YFRzeN2ATgzRkMD"
 
-print Object.Query.all()
+register(APPLICATION_ID, REST_API_KEY, master_key=None)
+
+class Dares(ParseObject):
+    pass
+
+class Posts(ParseObject):
+    pass
