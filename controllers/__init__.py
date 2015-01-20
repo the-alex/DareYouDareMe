11a08d562c231f2a6a8fcd8c55c04de892cd90c4
the-alex <thealex@umich.edu>
2015-01-20 15:55:57 -0500

    Fixed it to run locally

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -4,8 +4,6 @@ from account  import *
 from register import *
 from new_dare import *
 from about import *
-from venmoauth import *
 from logout import *
-from video import *
 from proof import *
 from verify import *
